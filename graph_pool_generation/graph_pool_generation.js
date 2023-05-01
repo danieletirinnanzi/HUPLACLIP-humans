55b31581d63758645587f4a0aa240b5b7cf1f62e
Daniele Tirinnanzi <danieletiri@gmail.com>
2023-05-01 17:54:41 +0200

    uploaded first graph pools (30 graphs per clique size)

--- graph_pool_generation/graph_pool_generation.js
+++ graph_pool_generation/graph_pool_generation.js
@@ -1,12 +1,14 @@
-//FUNCTION THAT GENERATES THE GRAPH POOL (one with clique, one without) THAT WILL BE DISPLAYED
-function generateGraphPool() {
+//FUNCTION THAT GENERATES GRAPHS WITH CLIQUE
+function generateGraphsWithClique() {
     /* INPUT: none ("currentExperiment" and "graphPoolObject" can be accessed from inside functions)
 
     OUTPUT:
-    - array that contains 100 couples of graphs with and without clique for each clique size (NB: triangular matrices are represented as objects)
-    STRUCTURE: [  first clique size: [ [{graph1}{graph2}] ],
-                  second block: [ [{graph1}{graph2}] [{graph1}{graph2}] [{graph1}{graph2}] ...(numberOfPresentationsPerBlock), ]
-                 ...(numberOfBlocks) ]
+    - array that has 100 graphs with clique for each clique size (NB: triangular matrices are represented as objects)
+    STRUCTURE: [    [ clique size n.1 --> {graph1} {graph2} ... {graph100} ]
+                    [ clique size n.2 --> {graph1} {graph2} ... {graph100} ]
+                    ...
+                    [ clique size n.15 --> {graph1} {graph2} ... {graph100} ] 
+                ]
     */
 
     /* Visual representation of a single triangular matrix object (example: N=5. NB: number of nodes start from zero):
@@ -27,23 +29,22 @@ function generateGraphPool() {
         }
     */
 
-    // empty array that will contain the couples of triangular matrices (objects) to be displayed
-    let graphsToDisplay = [];
+    // empty array that will contain all the graphs with the clique
+    let graphsWithClique = [];
 
-    // number of blocks:
-    for (let blockIndex = 0; blockIndex < currentExperiment.numberOfBlocks; blockIndex++) {
+    // number of clique sizes (from 0 to 30, in steps of 2):
+    for (let cliqueSizeIndex = 0; cliqueSizeIndex < (currentExperiment.arrayOfCliqueSizes.length); cliqueSizeIndex += 2) {
 
-        let graphsForCurrentBlock = []
+        let graphsForCurrentCliqueSize = []
 
-        // couples of graphs for a single block
-        for (let presentationIndex = 0; presentationIndex < currentExperiment.numberOfPresentationsPerBlock; presentationIndex++) {
+        // graphs for a single clique size:
+        for (let graphIndex = 0; graphIndex < graphPoolObject.numberOfGraphsPerCliqueSize; graphIndex++) {
 
-            // 1. Generating graph WITH CLIQUE:
             //empty object (the properties will be the nodes and the values will be the arrays that indicate the existing connections)
-            let graphWithClique = {};
+            let singleGraph = {};
             // randomly extracting the nodes that will be part of the clique
             let cliqueArray = [];
-            cliqueArray = createClique(currentExperiment.standardOrderOfNodes, currentExperiment.arrayOfCliqueSizes[presentationIndex])
+            cliqueArray = createClique(currentExperiment.standardOrderOfNodes, currentExperiment.arrayOfCliqueSizes[cliqueSizeIndex])
             // defining the values of the triangular adjacency matrix:
             for (let rowIndex = 1; rowIndex < currentExperiment.graphSize; rowIndex++) {
                 //defining empty array that will contain the connections for the current row
@@ -64,16 +65,77 @@ function generateGraphPool() {
                     }
                 }
                 //adding to the matrix information about the clique: size and array of nodes that are part of it
-                graphWithClique[currentExperiment.graphSize] = { 'clique_size': cliqueArray.length, 'clique_array': cliqueArray }
+                singleGraph[currentExperiment.graphSize] = { 'graph_name': `${graphIndex + 1}_N${currentExperiment.graphSize}_K${currentExperiment.arrayOfCliqueSizes[cliqueSizeIndex]}_CLIQUE`, 'clique_size': cliqueArray.length, 'clique_array': cliqueArray }
+
                 //adding to the object the property (the connections of the current node in the triangular matrix)
-                graphWithClique[rowIndex] = currentRowAssociations;
+                singleGraph[rowIndex] = currentRowAssociations;
             }
 
-            // 2. Generating graph WITHOUT CLIQUE:
+            // adding the graph to the array:
+            graphsForCurrentCliqueSize.push(singleGraph)
+        }
+
+        // adding array of graphs for current block to array of all graphs
+        graphsWithClique.push(graphsForCurrentCliqueSize)
+
+    }
+
+    return graphsWithClique
+
+}
+
+
+
+//FUNCTION THAT GENERATES GRAPHS WITHOUT CLIQUE
+function generateGraphsWithOutClique() {
+    /* INPUT: none ("currentExperiment" and "graphPoolObject" can be accessed from inside functions)
+
+    OUTPUT:
+    - array that has 100 graphs withOUT clique for each clique size
+    (indication of clique size is maintained because the probability of association is corrected in relation with to K)
+
+    STRUCTURE: [    [ clique size n.1 --> {graph1} {graph2} ... {graph100} ]
+                    [ clique size n.2 --> {graph1} {graph2} ... {graph100} ]
+                    ...
+                    [ clique size n.15 --> {graph1} {graph2} ... {graph100} ] 
+                ]
+    */
+
+    /* Visual representation of a single triangular matrix object (example: N=5. NB: number of nodes start from zero):
+    0|    -
+    1|   1 -
+    2|   0 1 -
+    3|   1 0 0 -
+    4|   0 0 1 0 -
+        ___________
+         0 1 2 3 4  
+
+        Corresponding object:
+        { 
+            1: [1],
+            2: [0 1],
+            3: [1 0 0],
+            4: [0 0 1 0]
+        }
+    */
+
+    // empty array that will contain all the graphs without the clique
+    let graphsWithoutClique = [];
+
+    // number of clique sizes (from 0 to 30, in steps of 2):
+    for (let cliqueSizeIndex = 0; cliqueSizeIndex < (currentExperiment.arrayOfCliqueSizes.length); cliqueSizeIndex += 2) {
+
+        let graphsForCurrentCliqueSize = []
+
+        // graphs for a single clique size:
+        for (let graphIndex = 0; graphIndex < graphPoolObject.numberOfGraphsPerCliqueSize; graphIndex++) {
+
             //empty object (the properties will be the nodes and the values will be the arrays that indicate the existing connections)
-            let graphWithoutClique = {};
+            let singleGraph = {};
             // increasing the value of p so that the two graphs will have the same average degree (pCorr = p + (K*(K-1)/N*(N-1)) * (1-p) )
-            let correctedProbabilityForCurrentGraph = currentExperiment.probabilityOfAssociation + (((currentExperiment.arrayOfCliqueSizes[presentationIndex] * (currentExperiment.arrayOfCliqueSizes[presentationIndex] - 1)) / (currentExperiment.graphSize * (currentExperiment.graphSize - 1))) * (1 - currentExperiment.probabilityOfAssociation))
+            let correctedProbabilityForCurrentGraph = currentExperiment.probabilityOfAssociation + (((currentExperiment.arrayOfCliqueSizes[cliqueSizeIndex] * (currentExperiment.arrayOfCliqueSizes[cliqueSizeIndex] - 1)) / (currentExperiment.graphSize * (currentExperiment.graphSize - 1))) * (1 - currentExperiment.probabilityOfAssociation))
+
+            console.log(currentExperiment.arrayOfCliqueSizes[cliqueSizeIndex])
 
             // defining the values of the triangular adjacency matrix:
             for (let rowIndex = 1; rowIndex < currentExperiment.graphSize; rowIndex++) {
@@ -88,24 +150,20 @@ function generateGraphPool() {
                     }
                 }
                 // adding to the matrix an indication about the absence of the clique:
-                graphWithoutClique[currentExperiment.graphSize] = "clique is absent"
+                singleGraph[currentExperiment.graphSize] = { 'graph_name': `${graphIndex + 1}_N${currentExperiment.graphSize}_K${currentExperiment.arrayOfCliqueSizes[cliqueSizeIndex]}_NOCLIQUE`, 'clique_size': currentExperiment.arrayOfCliqueSizes[cliqueSizeIndex], 'clique_array': "clique is absent" }
                 //adding to the object the property (the connections of the current node in the triangular matrix)
-                graphWithoutClique[rowIndex] = currentRowAssociations;
+                singleGraph[rowIndex] = currentRowAssociations;
             }
 
-            // Adding the two graphs to the array for the current trial (random choice of which one is the first in the couple) and pushing this array into the array relative to the current block
-            if (Math.round(Math.random())) {
-                graphsForCurrentBlock.push([graphWithClique, graphWithoutClique])
-            } else {
-                graphsForCurrentBlock.push([graphWithoutClique, graphWithClique])
-            }
+            // adding the graph to the array:
+            graphsForCurrentCliqueSize.push(singleGraph)
         }
 
         // adding array of graphs for current block to array of all graphs
-        graphsToDisplay.push(graphsForCurrentBlock)
+        graphsWithoutClique.push(graphsForCurrentCliqueSize)
 
     }
 
-    return graphsToDisplay
+    return graphsWithoutClique
 
 }
