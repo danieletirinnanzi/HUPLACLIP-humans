1aa298fe2caab3e294ebb480c38a6952a70d29dd
Daniele Tirinnanzi <danieletiri@gmail.com>
2023-08-03 12:21:14 +0200

    increased accepted vertical resolutions

--- device_requirements.js
+++ device_requirements.js
@@ -4,8 +4,8 @@ let physicalScreenWidth = screen.width * window.devicePixelRatio;
 let logicalPhysicalFraction = 1 / window.devicePixelRatio
 
 /* CHECKING DEVICE REQUIREMENTS and notifying user: */
-// 1. vertical resolution is at least 1080px
-let validVerticalResolutions = [1024, 1080, 1440, 1964]
+// 1. vertical resolution is at least 1024px
+let validVerticalResolutions = [1024, 1050, 1080, 1152, 1200, 1344, 1392, 1400, 1440, 1800, 1824, 1964]
 if (!validVerticalResolutions.includes(physicalScreenHeight)) {
     var confirmZoom = confirm(`If you are reading this message, make sure to SET YOUR BROWSER ZOOM TO 100% (you can change it in the browser options or by pressing "Ctrl +" / "Ctrl -" (Windows) or "⌥ ⌘ =" / "⌥ ⌘ -" (Mac) ) and click OK.\nThe page will automatically reload and this message should disappear.\n\nIf this message does not disappear after setting the browser zoom to 100%, the vertical resolution of your device is probably lower than 1024px.\nPlease open the experiment with a higher resolution device, and if you still encounter problems, get in touch with the experimenter at dtirinna@sissa.it.`);
     if (confirmZoom == true)
