ac0bc8c7d25e115426c854715a34ec1bc9796700
Daniele Tirinnanzi <danieletiri@gmail.com>
2024-08-19 14:50:43 +0200

    generated N0400 pool (no clique)

--- experiment_parameters.js
+++ experiment_parameters.js
@@ -42,10 +42,10 @@ switch (currentExperiment.graphSize) {
         currentExperiment.arrayOfCliqueSizes = [60, 60, 53, 53, 47, 47, 43, 43, 40, 40, 37, 37, 33, 33, 30, 30, 27, 27, 23, 23, 20, 20, 17, 17, 13, 13, 10, 10, 7, 7];
         break;
     case 300:
-
+        currentExperiment.arrayOfCliqueSizes = [90, 90, 80, 80, 70, 70, 65, 65, 60, 60, 55, 55, 50, 50, 45, 45, 40, 40, 35, 35, 30, 30, 25, 25, 20, 20, 15, 15, 10, 10];
         break;
     case 400:
-
+        currentExperiment.arrayOfCliqueSizes = [120, 120, 107, 107, 93, 93, 87, 87, 80, 80, 73, 73, 67, 67, 60, 60, 53, 53, 47, 47, 40, 40, 33, 33, 27, 27, 20, 20, 13, 13];
         break;
     case 480:
 
